e157daba428977d94c3eef2f18f65af3e77a5d63
Edson Rodrigues <143434065+ImaCod3r@users.noreply.github.com>
2026-03-13 23:33:27 +0000

    adding media queries

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,37 @@
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="w-full flex justify-between items-center">
             <a className="logo">
                 <img src={new URL('../assets/logo.png', import.meta.url).href} alt="" />
             </a>
 
-            <ul className="nav-links">
+            <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                ☰
+            </button>
+
+            <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
                 <li>
-                    <Link to="hero" smooth={true} duration={500}>
+                    <Link to="hero" smooth={true} duration={500} onClick={() => setIsOpen(false)}>
                         Início
                     </Link>
                 </li>
                 <li>
-                    <Link to="howitworks" smooth={true} duration={500}>
+                    <Link to="howitworks" smooth={true} duration={500} onClick={() => setIsOpen(false)}>
                         Como funciona
                     </Link>
                 </li>
                 <li>
-                    <Link to="organizers" smooth={true} duration={500}>
+                    <Link to="organizers" smooth={true} duration={500} onClick={() => setIsOpen(false)}>
                         Organizadores
                     </Link>
                 </li>
                 <li>
-                    <Link to="contact" smooth={true} duration={500}>
+                    <Link to="contact" smooth={true} duration={500} onClick={() => setIsOpen(false)}>
                         Contactos
                     </Link>
                 </li>
